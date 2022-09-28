22e89793de51541433f2ebb7cf88c7964047ad14
Dreamyplayer <saikumarchinna123@gmail.com>
2022-09-29 03:00:41 +0530

    fix: rename filenames

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 const generator = {
-  length: 24,
+  length: 16,
   random: true,
   amount: 10000,
-  outfile: "./db/codes/codes.txt",
+  outfile: './db/genrated_codes.txt',
 };
 
 const checker = {
   interval: 50,
   updateRate: 1000,
-  proxiesType: "http",
+  proxiesType: 'http',
   proxy: true,
   autoGrabProxies: true,
   debug: false,
-  proxiesfile: "./db/proxies/proxies.txt",
-  codesfile: "./db/codes/codes.txt",
+  proxiesfile: './db/proxies.txt',
+  codesfile: './db/genrated_codes.txt',
 
-  bURL: "https://discord.com/api/v9/entitlements/gift-codes/",
-  params: "?with_application=false&with_subscription_plan=true",
+  bURL: 'https://discord.com/api/v9/entitlements/gift-codes/',
+  params: '?with_application=false&with_subscription_plan=true',
 };
 
 module.exports = { generator, checker };
